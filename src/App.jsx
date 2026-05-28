ced9624375d305d0a42fd81f49df147319c3be66
Felipe Silva de Sa Ferreira <rm98906@estudante.fieb.edu.br>
2026-05-28 09:13:24 -0300

    arquivos, home e menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
-
+import HomeFuncionario from './pages/HomeFuncionario/HomeFuncionario'
 function App() {
   return (
-    <>
-     
+    <><div>
 
+
+
+     <HomeFuncionario  />
+     
+     
+     
+     </div>
     </>
   )
 }
---
+++ src/pages/HomeFuncionario/HomeFuncionario.jsx
@@ -0,0 +1,15 @@
+import MenuFuncionario from "../MenuFuncionario/MenuFuncionario";
+
+const HomeFuncionario= () => {
+    return (
+       
+       
+           <div>
+            
+            <MenuFuncionario/>
+           </div> 
+           
+        )
+
+    }
+ export default HomeFuncionario;
