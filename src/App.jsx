e3649364bf8ef7299853322d7a98d0233d26b99b
Felipe Silva de Sa Ferreira <rm98906@estudante.fieb.edu.br>
2026-06-03 09:56:08 -0300

    sistema de rotas em construção

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import './App.css'
+//import './App.css'
 import HomeFuncionario from './pages/HomeFuncionario/HomeFuncionario'
 function App() {
   return (
-    <><div>
 
 
-
-     <HomeFuncionario  />
-     
-     
-     
+    <>
+     <div>
+        <HomeFuncionario  />
      </div>
     </>
   )
